fef2fbb5200409f571e52b0761a9a77bac53ed78
Peter ZNameroski <pznameroski@gmail.com>
2012-01-17 16:36:54 -0600

    first commit

---
+++ FS/FindStrings.h
@@ -0,0 +1,17 @@
+/*
+ * FindStrings.h
+ *
+ *  Created on: Jan 12, 2012
+ *      Author: Peter
+ */
+
+#ifndef FINDSTRINGS_H_
+#define FINDSTRINGS_H_
+
+class FindStrings {
+public:
+	FindStrings();
+	virtual ~FindStrings();
+};
+
+#endif /* FINDSTRINGS_H_ */
